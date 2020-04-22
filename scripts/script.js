548fed128c091f73995135ee9747a6b5a298819e
Alessandro9300 <aandreoli93@gmail.com>
2020-04-22 21:09:12 +0200

    aggiunte bandiere

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
+// Trasformiamo il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+// Trasformiamo poi la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+//
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
 
 
 $(document).ready(function(){
@@ -14,8 +13,7 @@ $(document).ready(function(){
 
   queryButton.click(function(){
     var queryVal = $("#input-search").val();
-    $(".films").find(".movie").remove()
-    $(".films").find("p").remove()
+    $(".films").html("");
 
     $.ajax({
       url: "https://api.themoviedb.org/3/search/movie/",
@@ -27,7 +25,7 @@ $(document).ready(function(){
 
       },
       success: function(data){
-
+        var bandiera;
         var arrayResults = data.results;
         console.log(arrayResults);
 
@@ -37,16 +35,26 @@ $(document).ready(function(){
 
           for (var i = 0; i < arrayResults.length; i++) {
 
+            if (arrayResults[i].original_language == "it"){
+              bandiera = '<img id="bandiera" src="img/italia.gif" alt="">'
+            } else if (arrayResults[i].original_language == "en") {
+              bandiera = '<img id="bandiera" src="img/inghilterra.png" alt="">';
+            } else {
+              bandiera = arrayResults[i].original_language;
+            }
+
             var objTemplate = {
               titolo: arrayResults[i].title,
               titoloOriginale: arrayResults[i].original_title,
-              lingua: arrayResults[i].original_language,
+              lingua: bandiera,
               voto: arrayResults[i].vote_average,
               img: "https://image.tmdb.org/t/p/w220_and_h330_face" + arrayResults[i].poster_path
 
             }
 
             console.log(objTemplate.img);
+
+
 
             if (arrayResults[i].poster_path == null){
               objTemplate.img = "img/question-mark.png"
